e88466b4329c0e3996bd2c635613f57c118041fe
Rahul Kumar <68834624+pcrahul702@users.noreply.github.com>
2024-03-13 14:37:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bucket_name="rahul-kr-02-01-2002"
 
 console.log(process.env.KEY)
 console.log(process.env.SECRET_KEY);
-const KEY="AKIASCSJ7SHABORTW27O"
-const SECRET_KEY="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"
+const KEY=""
+const SECRET_KEY=""
 
 const s3=new AWS.S3({
     accessKeyId:KEY,
